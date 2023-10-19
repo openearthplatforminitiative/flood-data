700cd63e29dcb5edbfb2b0ecda9623f35dd52d86
gil.tinde@knowit.no <gil.tinde@knowit.no>
2023-10-19 10:29:12 +0000

    Finish first version of filtering and conversion notebook

--- databricks/grib-filtering-and-conversion.py
+++ databricks/grib-filtering-and-conversion.py
@@ -1,18 +1,42 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC ### Discharge (GRIB data) filtering by upstream area and saving as Parquet
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC **Imports**
+
+# COMMAND ----------
+
 import xarray as xr
 from datetime import datetime, timedelta
 from flood.etl.utils import load_dataset
 from flood.etl.filter_by_upstream import apply_upstream_threshold
 from flood.etl.raster_converter import RasterConverter
 
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC **Hardcoded configuration parameters (should be placed in config file in the future)**
 
 # COMMAND ----------
+
 
 ROI_CENTRAL_AFRICA = {'lat_min': -6.0, 'lat_max': 17.0,
                       'lon_min': -18.0, 'lon_max': 52.0}
 RESOLUTION = 0.05
 BUFFER = RESOLUTION / 4
 UPSTREAM_THRESHOLD = 250000 # m^2
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC **Define path to read discharge from and load GRIB dataset**
 
 # COMMAND ----------
 
@@ -80,3 +104,17 @@ filtered_df = converter.dataset_to_dataframe(filtered_ds['dis24'],
                                              cols_to_drop=['surface'], 
                                              drop_na_subset=['dis24'], 
                                              drop_index=False)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC **Save to Parquet**
+
+# COMMAND ----------
+
+filtered_parquet_filename = f'filtered_{year}_{month}_{day}_{leadtime_hour}.parquet'
+filtered_parquet_folder = f'data/{year}-{month}-{day}/'
+filtered_parquet_folder = ''
+filtered_parquet_file_path = filtered_parquet_folder + filtered_parquet_filename
+converter.dataframe_to_parquet(filtered_df, filtered_parquet_file_path)
