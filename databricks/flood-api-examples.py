dc4720238201c4e103ff7e50da8ee1f6348c149b
gil.tinde@knowit.no <gil.tinde@knowit.no>
2023-10-31 15:33:57 +0000

    Improved logic for fetching grid cells based on queried point

--- databricks/flood-api-examples.py
+++ databricks/flood-api-examples.py
@@ -17,7 +17,8 @@ from flood.utils.config import get_config_val
 import geopandas as gpd
 import pandas as pd
 from shapely import wkt 
-from shapely.geometry import Point
+from shapely.geometry import Polygon
+from math import floor
 
 # COMMAND ----------
 
@@ -34,6 +35,7 @@ GLOFAS_RET_PRD_THRESH_VALS = get_config_val("GLOFAS_RET_PRD_THRESH_VALS")
 GLOFAS_PRECISION = get_config_val("GLOFAS_PRECISION")
 GLOFAS_RESOLUTION = get_config_val('GLOFAS_RESOLUTION')
 GLOFAS_PROCESSED_THRESH_FILENAME = get_config_val("GLOFAS_PROCESSED_THRESH_FILENAME")
+GLOFAS_ROI_CENTRAL_AFRICA = get_config_val("GLOFAS_ROI_CENTRAL_AFRICA")
 
 S3_GLOFAS_PROCESSED_PATH = get_config_val("S3_GLOFAS_PROCESSED_PATH")
 GLOFAS_PROCESSED_SUMMARY_FORECAST_FILENAME = get_config_val("GLOFAS_PROCESSED_SUMMARY_FORECAST_FILENAME")
@@ -45,25 +47,99 @@ GLOFAS_FLOOD_PEAK_TIMINGS = get_config_val('GLOFAS_FLOOD_PEAK_TIMINGS')
 
 # COMMAND ----------
 
-date = datetime.utcnow() #- timedelta(days=3)
+lat_min = GLOFAS_ROI_CENTRAL_AFRICA['lat_min']
+lat_max = GLOFAS_ROI_CENTRAL_AFRICA['lat_max']
+lon_min = GLOFAS_ROI_CENTRAL_AFRICA['lon_min']
+lon_max = GLOFAS_ROI_CENTRAL_AFRICA['lon_max']
+
+# COMMAND ----------
+
+date = datetime.utcnow() #- timedelta(days=1)
 formatted_date = date.strftime("%Y-%m-%d")
 formatted_date
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC
+# MAGIC **Define functionality**
+
+# COMMAND ----------
+
 def read_with_geopandas(file_path):
+    """
+    Reads a Parquet file into a GeoDataFrame.
+    """
 
     # Read Parquet into Pandas DataFrame
     df = pd.read_parquet(file_path)
 
     # Convert WKT strings to geometry objects
     df['geometry'] = df['wkt'].apply(wkt.loads)
-    df = gpd.GeoDataFrame(df, geometry='geometry')
-
-    # Optionally, you can drop the 'wkt' column now
-    df.drop(columns='wkt', inplace=True)
+    df = gpd.GeoDataFrame(df, geometry='geometry').drop(columns='wkt')
 
     return df
+
+def get_grid_cell_bounds(lat, lon, grid_size=0.05, precision=3):
+    """
+    Given a latitude and longitude, find the bounding box of the grid cell
+    it falls into.
+
+    The function identifies the grid cell based on the following logic:
+    - If the provided point is on the boundary between two cells, the function 
+      will assign the point to the cell to its east (right, for longitude) or 
+      north (above, for latitude).
+    - For example, for a grid_size of 0.05:
+      * An input latitude of -5.8 and longitude of 37.75 would result in a 
+        bounding box from latitude -5.8 to -5.75 and longitude 37.75 to 37.8.
+      * An input latitude of -5.81 and longitude of -37.7501 would result in a 
+        bounding box from latitude -5.85 to -5.8 and longitude -37.8 to -37.75.
+
+    Parameters:
+    - lat (float): The latitude of the point.
+    - lon (float): The longitude of the point.
+    - grid_size (float, optional): The size of each grid cell. Defaults to 0.05.
+    - precision (int, optional): Number of decimal places to round to. Defaults to 3.
+
+    Returns:
+    tuple: Bounding box of the grid cell as (min_latitude, max_latitude, min_longitude, max_longitude).
+    """
+
+    # Calculate lower boundaries using math.floor
+    min_lon_cell = floor(lon / grid_size) * grid_size
+    min_lat_cell = floor(lat / grid_size) * grid_size
+
+    # Use the grid size to find the upper boundaries
+    max_lon_cell = min_lon_cell + grid_size
+    max_lat_cell = min_lat_cell + grid_size
+
+    # Round the results
+    return round(min_lat_cell, precision),\
+           round(max_lat_cell, precision),\
+           round(min_lon_cell, precision),\
+           round(max_lon_cell, precision)
+
+def create_polygon_from_bounds(min_lat, max_lat, 
+                               min_lon, max_lon, 
+                               buffer=0, precision=3):
+    """
+    Given the bounds (min_lat, max_lat, min_lon, max_lon), 
+    return a Polygon.
+    """
+
+    # Define the four corners of the polygon
+    bottom_left = (round(min_lon - buffer, precision), 
+                   round(min_lat - buffer, precision))
+    bottom_right = (round(max_lon + buffer, precision), 
+                    round(min_lat - buffer, precision))
+    top_right = (round(max_lon + buffer, precision), 
+                 round(max_lat + buffer, precision))
+    top_left = (round(min_lon - buffer, precision), 
+                round(max_lat + buffer, precision))
+
+    # Create and return the polygon
+    return Polygon([bottom_left, top_left, top_right, 
+                    bottom_right, bottom_left])
 
 # COMMAND ----------
 
@@ -76,24 +152,7 @@ df_summary = read_with_geopandas(summary_df_file_path)
 
 detailed_df_file_path = os.path.join(PYTHON_PREFIX, S3_GLOFAS_PROCESSED_PATH, 
                                      formatted_date, GLOFAS_PROCESSED_DETAILED_FORECAST_FILENAME)
-
 df_detailed = read_with_geopandas(detailed_df_file_path)
-
-# COMMAND ----------
-
-df_summary.info()
-
-# COMMAND ----------
-
-df_summary.head(5)
-
-# COMMAND ----------
-
-df_detailed.info()
-
-# COMMAND ----------
-
-df_detailed.head(5)
 
 # COMMAND ----------
 
@@ -111,66 +170,64 @@ lon_set_df2 = set(df_detailed['longitude'])
 are_latitudes_equal = lat_set_df1 == lat_set_df2
 are_longitudes_equal = lon_set_df1 == lon_set_df2
 
-print(are_latitudes_equal)  # True
-print(are_longitudes_equal)  # True
-
-# COMMAND ----------
-
-# Define a very small offset for edge cases
-# when a point is on a cell border or corner
-EPSILON = 1e-9
-PRECISION = 8 # one less than the epsilon
+print(are_latitudes_equal)  # Should be True
+print(are_longitudes_equal)  # Should be True
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC
-# MAGIC **Query a point**
+# MAGIC **Query a point with possibility to fetch neighboring cells**
 
 # COMMAND ----------
 
-# Given lat/lon coordinates
-user_lat, user_lon = 16.50, 28.73
-
-# Round to 8 decimal places and adjust with epsilon. 
-# This ensures that the queried lat, lon will never fall on the
-# border between two cells (or at the corner between
-# four cells). As a result, if the user queries latitude = 17.0 and
-# longitude = 16.55, the considered cell will be to the bottom right,
-# i.e., the one with center (16.975, 16.575).
-lat, lon = round(user_lat, PRECISION), round(user_lon, PRECISION)
-
-# Adjust the point slightly
-point = Point(lon + EPSILON, lat - EPSILON)
-
-# Use the contains method to filter the dataframe
-filtered_summary_df = df_summary[df_summary['geometry'].contains(point)]
+# Assume the user provides this in the query
+user_lat, user_lon, fetch_neighbors = -5.81, -37.7501, True
 
 # COMMAND ----------
 
-filtered_summary_df
+# Get the bounds for the queryed cell
+cell_bounds = get_grid_cell_bounds(lat=user_lat, lon=user_lon, 
+                                   grid_size=GLOFAS_RESOLUTION, 
+                                   precision=GLOFAS_PRECISION)
+
+# Get the deflated geometry to find primary cell
+reduced_geometry = create_polygon_from_bounds(*cell_bounds, 
+                                              buffer=-GLOFAS_RESOLUTION/2,
+                                              precision=GLOFAS_PRECISION)
+
+if fetch_neighbors:
+    print('fetching primary cell and neighbors')
+
+    # Get the inflated geometry to find primary cell and neighbors
+    expanded_geometry = create_polygon_from_bounds(*cell_bounds, 
+                                               buffer=GLOFAS_RESOLUTION/2,
+                                               precision=GLOFAS_PRECISION)
+    
+    # Query the summary dataframe
+    all_cells_df = df_summary[df_summary['geometry'].intersects(expanded_geometry)]
+
+    # Define mask for distinguishing between primary cell and neighbors
+    primary_cell_mask = all_cells_df['geometry'].intersects(reduced_geometry)
+
+    # Keep primary cell
+    primary_cell_df = all_cells_df[primary_cell_mask]
+
+    # Keep only neighboring cells
+    neighbors_only_df = all_cells_df[~primary_cell_mask]
+else:
+    print('Fetching only primary cell')
+    # Query the dataframe for the primary cell
+    primary_cell_df = df_summary[df_summary['geometry'].intersects(reduced_geometry)]
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC
-# MAGIC **Fetch neighboring cells**
+all_cells_df
 
 # COMMAND ----------
 
-# Assuming the primary cell has already been found and it exists
-primary_cell_geometry = filtered_summary_df.iloc[0].geometry
-
-# Get the bounding box (envelope) of the primary cell and expand it
-# Assuming GLOFAS_RESOLUTION / 2 is the half-length of a cell side
-expanded_bbox = primary_cell_geometry.envelope.buffer(1.5 * GLOFAS_RESOLUTION / 2)
-
-# Query using the expanded bounding box to get neighbors
-neighbors_df = df_summary[df_summary['geometry'].intersects(expanded_bbox)]
-
-# To exclude the primary cell from the result
-neighbors_df = neighbors_df[neighbors_df.geometry != primary_cell_geometry]
+primary_cell_df
 
 # COMMAND ----------
 
-neighbors_df
+neighbors_only_df
