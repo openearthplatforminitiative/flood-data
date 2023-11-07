e260cf0e17334f77909081d527cda9193a00e096
gil.tinde@knowit.no <gil.tinde@knowit.no>
2023-11-07 12:39:00 +0000

    Fix incorrect upstream area threshold
    250 km^2 is 1,000,000 m^2, not 1,000 m^2...

--- flood/etl/filter_by_upstream.py
+++ flood/etl/filter_by_upstream.py
@@ -1,7 +1,7 @@
 from flood.etl.utils import open_dataset, restrict_dataset_area
 
 def get_filtered_discharge_from_files(discharge_file_path, upstream_file_path, 
-                                      threshold_area=250000, discharge_engine="cfgrib", 
+                                      threshold_area=250*1e6, discharge_engine="cfgrib", 
                                       upstream_engine="netcdf4"):
 
     ds_discharge = open_dataset(discharge_file_path, engine=discharge_engine)
@@ -9,7 +9,7 @@ def get_filtered_discharge_from_files(discharge_file_path, upstream_file_path,
 
     return apply_upstream_threshold(ds_discharge, ds_upstream, threshold_area=threshold_area)
 
-def apply_upstream_threshold(ds_discharge, ds_upstream, threshold_area=250000, buffer=0.0125):
+def apply_upstream_threshold(ds_discharge, ds_upstream, threshold_area=250*1e6, buffer=0.0125):
 
     subset_uparea = restrict_dataset_area(ds_upstream['uparea'],
                                           ds_discharge.latitude.min(),
